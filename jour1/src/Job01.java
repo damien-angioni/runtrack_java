b537ef05a29fa086d2822f265416d73f474259e2
damien-angioni <115216654+damien-angioni@users.noreply.github.com>
2023-09-06 10:49:30 +0200

    Job01

---
+++ jour1/src/Job01.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Job01
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Say your name: ");
+        String str= sc.nextLine();
+        System.out.print("You goddam right, "+str+"!");
+    }
+}
