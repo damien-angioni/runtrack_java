bed4447a18525c558c7563b2078f615f07a68548
damien-angioni <115216654+damien-angioni@users.noreply.github.com>
2023-09-07 09:18:44 +0200

    Job 07 Done

---
+++ jour1/src/Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    public static void main(String[] args) {
+        float radius = 5;
+        double area = Math.PI * (radius * radius);
+        System.out.println("the area of a circle of " + radius + " radius is " + area);
+    }
+}
---
+++ jour1/src/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public static void main(String[] args){
+        float side= 5;
+        float area= side*side;
+        System.out.println("the area of a square of "+side+" side is "+area);
+    }
+}
+
